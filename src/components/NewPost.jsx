2e3d6278b6a6de2d7aa1705b7903e66f7694c90c
Fandi16 <96216814+Fandi16@users.noreply.github.com>
2023-11-01 08:09:34 +0700

    Update NewPost.jsx

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -3,6 +3,7 @@ import classes from './NewPost.module.css';
 function NewPost() {
     return (
         <form className={ classes.form }>
+            <h1>KHOIR AFANDI (20670121)</h1>
             <p>
                 <label htmlFor="body">Text</label>
                 <textarea  id="body" required rows={3}></textarea>
